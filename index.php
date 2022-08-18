ce8003efe0da897e87e56af33a8a6b541f7c1d2e
Adil Ali <aadilalipk@gmail.com>
2022-08-18 22:20:11 +0500

    first Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api.bigcommerce.com/stores/cxahscodbs/v2/customers/1",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "Content-Type: application/json",
+    "X-Auth-Token: cfxi39as2eounkznn21w3q3z1nkfwgr"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo json_encode( array("status" => "failed", "res" => "cURL Error #:" . $err) );
+} else {
+   echo json_encode( array("status" => "success", "res" => $response) );
+}
