e586a67e169ad32da4bb2d572eb54967e07f1ec7
Adil Ali <aadilalipk@gmail.com>
2022-08-18 23:37:01 +0500

    add html rresposne jso parse data.res COR

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Max-Age: 1000");
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding, X-Auth-Token");
 header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 
 $curl = curl_init();
